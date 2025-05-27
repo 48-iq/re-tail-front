0a65d024b385962d572ee5c8e2e010514c49962e
48-iq <ivn25ov@mail.ru>
2025-05-28 02:02:20 +0400

    save changes

---
+++ src/shared/types/AnnouncmentCardType.ts
@@ -0,0 +1,14 @@
+export type AnnouncmentCardType = {
+  id: string
+  title: string
+  creatorId: string
+  isActive: boolean
+  previewImageId: string
+  subcategoryName: string
+  categoryName: string
+  cityName: string
+  categoryId: string
+  subcategoryId: string
+  cityId: string  
+  price: number
+}
---
+++ src/ui/components/announcment-card/CardPreviewImage.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="announcment-card__preview-image" 
+    :src="`${HOST}/images/${previewImageId}`" 
+    alt="preview image"
+  >
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { HOST } from '@/shared/variables/host';
+const props = defineProps<{
+  previewImageId: string
+}>();
+</script>
+
+<style scoped>
+  .preview-image {
+    width: 12.5rem;
+    height: 12.5rem;
+  }
+</style>
